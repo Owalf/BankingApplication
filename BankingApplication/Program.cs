af3da148fbae1fa614bb32a04db73b3f462b771f
BuildTools <unconfigured@null.spigotmc.org>
2020-10-15 19:30:17 -0400

    Added changes to Program.cs

--- BankingApplication/Program.cs
+++ BankingApplication/Program.cs
@@ -10,8 +10,57 @@ namespace BankingApplication
     {
         static void Main(string[] args)
         {
-            string Option;
-            
+            SavingsAccount savings = new SavingsAccount(5.00, .15);
+            ChequingAccount chequing = new ChequingAccount(5.00, .05);
+            GlobalSavingsAccount globalsavings = new GlobalSavingsAccount(5.00, .15);
+
+            Console.WriteLine("Welcome to OmegaBank, please select an account type.");
+            Console.WriteLine("A: Savings");
+            Console.WriteLine("B: Checking");
+            Console.WriteLine("C: GlobalSavings");
+            Console.WriteLine("Q: Exit");
+            string AccountType = Console.ReadLine();
+
+            switch(AccountType.ToUpper())
+            {
+                case "A":
+                    Console.WriteLine("Savings Menu");
+                    Console.WriteLine("A: Deposit");
+                    Console.WriteLine("B: Withdraw");
+                    Console.WriteLine("C: Close + Report");
+                    Console.WriteLine("R: Return to Bank Menu");
+
+                    string Option = Console.ReadLine();
+
+                    switch(Option.ToUpper())
+                    {
+                        case "A":
+                            Console.WriteLine("How much do you want to deposit?");
+                            double amount = Console.ReadLine();
+                            SavingsAccount.MakeDeposit(amount);
+                    }
+                    break;
+                case "B":
+                    Console.WriteLine("Savings Menu");
+                    Console.WriteLine("A: Deposit");
+                    Console.WriteLine("B: Withdraw");
+                    Console.WriteLine("C: Close + Report");
+                    Console.WriteLine("R: Return to Bank Menu");
+                    break;
+                case "C":
+                    Console.WriteLine("Savings Menu");
+                    Console.WriteLine("A: Deposit");
+                    Console.WriteLine("B: Withdraw");
+                    Console.WriteLine("C: Close + Report");
+                    Console.WriteLine("D: Report Balance in USD");
+                    Console.WriteLine("R: Return to Bank Menu");
+                    break;
+                case "Q":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }

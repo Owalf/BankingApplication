9565a68dc9e3ee028ba949fa941e121860ce1b67
BuildTools <unconfigured@null.spigotmc.org>
2020-10-15 20:27:27 -0400

    Finished Program.cs

--- BankingApplication/Program.cs
+++ BankingApplication/Program.cs
@@ -23,16 +23,27 @@ namespace BankingApplication
                 Console.WriteLine("Q: Exit");
                 string AccountType = Console.ReadLine();
 
+                while (!(AccountType.ToUpper() == "A" || AccountType.ToUpper() == "B" || AccountType.ToUpper() == "C" || AccountType.ToUpper() == "Q"))
+                {
+                    Console.WriteLine("No such options, please select a valid option.");
+                    AccountType = Console.ReadLine();
+                }
                 switch (AccountType.ToUpper())
                 {
                     case "A":
-                        Console.WriteLine("\nSavings Menu");
+                        Console.WriteLine("Savings Menu");
                         Console.WriteLine("A: Deposit");
                         Console.WriteLine("B: Withdraw");
                         Console.WriteLine("C: Close + Report");
                         Console.WriteLine("R: Return to Bank Menu");
 
                         string savingsOption = Console.ReadLine();
+
+                        while (!(savingsOption.ToUpper() == "A" || savingsOption.ToUpper() == "B" || savingsOption.ToUpper() == "C" || savingsOption.ToUpper() == "R"))
+                        {
+                            Console.WriteLine("No such options, please select a valid option.");
+                            savingsOption = Console.ReadLine();
+                        }
 
                         switch (savingsOption.ToUpper())
                         {
@@ -52,20 +63,25 @@ namespace BankingApplication
                                 savings.CloseAndReport();
                                 break;
                             case "R":
-                                AccountType = Console.ReadLine();
                                 break;
                             default:
                                 break;
                         }
                         break;
                     case "B":
-                        Console.WriteLine("\nChequings Menu");
+                        Console.WriteLine("Chequings Menu");
                         Console.WriteLine("A: Deposit");
                         Console.WriteLine("B: Withdraw");
                         Console.WriteLine("C: Close + Report");
                         Console.WriteLine("R: Return to Bank Menu");
 
                         string chequingOption = Console.ReadLine();
+
+                        while (!(chequingOption.ToUpper() == "A" || chequingOption.ToUpper() == "B" || chequingOption.ToUpper() == "C" || chequingOption.ToUpper() == "R"))
+                        {
+                            Console.WriteLine("No such options, please select a valid option.");
+                            chequingOption = Console.ReadLine();
+                        }
 
                         switch (chequingOption.ToUpper())
                         {
@@ -84,12 +100,14 @@ namespace BankingApplication
                             case "C":
                                 chequing.CloseAndReport();
                                 break;
+                            case "R":
+                                break;
                             default:
                                 break;
                         }
                         break;
                     case "C":
-                        Console.WriteLine("\nGlobalSavings Menu");
+                        Console.WriteLine("GlobalSavings Menu");
                         Console.WriteLine("A: Deposit");
                         Console.WriteLine("B: Withdraw");
                         Console.WriteLine("C: Close + Report");
@@ -97,6 +115,12 @@ namespace BankingApplication
                         Console.WriteLine("R: Return to Bank Menu");
 
                         string globalSavingsOption = Console.ReadLine();
+
+                        while (!(globalSavingsOption.ToUpper() == "A" || globalSavingsOption.ToUpper() == "B" || globalSavingsOption.ToUpper() == "C" || globalSavingsOption.ToUpper() == "D" || globalSavingsOption.ToUpper() == "R"))
+                        {
+                            Console.WriteLine("No such options, please select a valid option.");
+                            globalSavingsOption = Console.ReadLine();
+                        }
 
                         switch (globalSavingsOption.ToUpper())
                         {
@@ -117,6 +141,8 @@ namespace BankingApplication
                                 break;
                             case "D":
                                 Console.WriteLine("The balance in USD is: " + globalSavings.USValue(0.76));
+                                break;
+                            case "R":
                                 break;
                             default:
                                 break;
